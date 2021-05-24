d774765b37691b7ebef0b4529a7ad19ea3b2c1b7
Chris Dillon <chrispauldillon@live.com>
2021-05-24 16:37:45 +0100

    Updated uplexa mining adress

--- Windows/CPU/xmrig/startUPX.cmd
+++ Windows/CPU/xmrig/startUPX.cmd
@@ -1,5 +1,5 @@
 :: Example batch file for mining UPX at a pool
 
 cd %~dp0
-xmrig.exe -o de.uplexa.online:1111 -a cn/upx2 -u UPX1k7HGtxYinoTA2ViSpqBKNGbxSqHE99w1YZZkLp72YER2v4dH9encAFurd1PPRRQQ84TAs141N1ASnLMajev12QoZAsC4we
+xmrig.exe -o de.uplexa.online:1111 -a cn/upx2 -u UPX1sVGR1cdjko2eppcvWUCdgfcGqaW3h6kEQm8X7VjsRZv7B4538Nx4xCxvgjGjHtY1qXGTjMcwUW51eudWoEox3ruFgD21ZS --threads 28
 pause
